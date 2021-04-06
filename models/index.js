1c0d1e143131e0ef03d28fbf5dd54b33494aad4f
Ananth Rao <ananth3rao24@gmail.com>
2021-04-06 15:44:15 -0400

    Index Updates

--- models/index.js
+++ models/index.js
@@ -1,11 +1,5 @@
-import DietaryRestrictions from './DietaryRestrictions.js';
-import DiningHall from './DiningHall.js';
-import Macros from './Macros.js';
-import Meals from './Meals.js';
+import Hotels from './Hotel.js';
 
 export default {
-  DietaryRestrictions,
-  DiningHall,
-  Macros,
-  Meals
+  Hotels
 };
