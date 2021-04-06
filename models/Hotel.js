d7cee7e6d2661759590f1cd10deb488f76ae00ff
Ananth Rao <ananth3rao24@gmail.com>
2021-04-06 15:02:43 -0400

    Adding variables to hotel.js

---
+++ models/Hotel.js
@@ -0,0 +1,43 @@
+export default (sequelize, DataTypes) => {
+    const Hotels = sequelize.define(
+        'Hotels',
+        {
+            hotel_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                unique: true,
+                primaryKey: true
+            },
+            hotel_name: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            hotel_phone_number: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            sub_region_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false
+            },
+            street_address: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            city: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            zip_code: {
+               type: DataTypes.STRING,
+               allowNull: false
+            },
+            state: {
+                type: DataTypes.STRING,
+                allowNull: false
+            }
+            
+        }
+    );
+    return Hotels
+}
