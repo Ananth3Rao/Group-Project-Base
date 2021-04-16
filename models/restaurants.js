d0a0cc0a3545993c40c5023171f0b329d80f6651
Emmanuel933 <yawadusei93@gmail.com>
2021-04-16 16:06:49 -0400

    restaurants models

---
+++ models/restaurants.js
@@ -0,0 +1,86 @@
+/* eslint-disable indent */
+/* eslint-disable linebreak-style */
+export default (sequelize, DataTypes) => {
+    const Comments = sequelize.define(
+        'restaurants',
+        {
+            restaurant_id: {
+                type: DataTypes, INTERGER,
+                allowNull: false,
+                unique: true,
+                primaryKey: true
+            },
+            sub_region_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false
+            },
+            price_range: {
+                type: DataTypes.VARCHAR,
+                allowNull: false
+            },
+            cuisine_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false
+            },
+            rating: {
+                type: DataTypes.INTEGER,
+                allowNull: false
+            },
+            open_time: {
+                type: DataTypes.TIME,
+                allowNull: false
+            },
+            close_time: {
+                type: DataTypes.TIME,
+                allowNull: false
+            },
+            phone_number: {
+                type: DataTypes.VARCHAR,
+                allowNull: false
+            },
+            street_address:{
+                type: DataTypes.VARCHAR,
+                allowNull: false
+            },
+            city: {
+                type: DataTypes.VARCHAR,
+                allowNull: false
+            },
+            zip_code: {
+                type: DataTypes.VARCHAR,
+                allowNull: false
+            },
+            state: {
+                type: DataTypes.CHAR,
+                allowNull: false
+            },
+            atmosphere: {
+                type: DataTypes.VARCHAR,
+                allowNull: false
+            },
+            payment_accepted: {
+                type: DataTypes.VARCHAR,
+                allowNull: false
+            },
+            { freezeTableName: true, timestamps: false }
+            );
+            return restaurants;
+        }
+
+
+
+        
+
+
+
+
+
+
+zip_code
+varchar(45)
+state
+char(2)
+atmosphere
+varchar(45)
+accepted_payment
+varchar(45)
