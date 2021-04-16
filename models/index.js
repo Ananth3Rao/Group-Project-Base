18a6e6083add774f4b66b32d4c787dc894e5094c
kvn175 <73406755+kvn175@users.noreply.github.com>
2021-04-16 14:32:45 -0400

    created rooms.js and updated index.js for Beds and Rooms

--- models/index.js
+++ models/index.js
@@ -3,11 +3,16 @@ import Comments from './Comments.js';
 import Locations from './Locations.js';
 import Macros from './Macros.js';
 import Amenities from './Amenities.js';
+import Beds from './beds.js';
+import Rooms from './rooms.js';
 
 export default {
   Hotels,
   Comments,
   Locations,
   Amenities,
-  Macros
+  Macros,
+  Beds,
+  Rooms
+
 };
