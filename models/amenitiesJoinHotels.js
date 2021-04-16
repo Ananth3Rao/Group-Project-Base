130350b64f8c739bdd549c5721307c7908acc072
eurzua <eurzua@umd.edu>
2021-04-15 20:01:03 -0400

    updated the join table model

--- models/amenitiesJoinHotels.js
+++ models/amenitiesJoinHotels.js
@@ -1,17 +1,25 @@
+/* I found the following code online @ https://sequelize.org/master/manual/assocs.html#creating-the-standard-relationships
+this is my best interpreation of it */
 export default (sequelize, DataTypes) => {
-  const amenitiesJoinHotels = sequelize.define(
-    'hotels_amenities_join',
-    {
-      hotel_id: {
-        type: DataTypes.INTEGER, /* this is a foreing key*/
-        allowNull: false,
-      },
-      amenities_id: {
-        type: DataTypes.INTEGER, /*this is a foreign key */
-        allowNull: false,
+  const Hotels = sequelize.define('hotel_overview', { name: DataTypes.INTEGER });
+  const Amenities = sequelize.define('hotels_amenities_join', { name: DataTypes.INTEGER });
+  const amenitiesJoinHotels = sequelize.define('amenitiesJoinHotels', {
+    hotel_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Hotels,
+        key: 'id'
       }
     },
-    { freezeTableName: true, timestamps: false }
-  );
+    amenities_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Amenities
+      }
+    }
+  },
+  { freezeTableName: true, timestamps: false });
   return amenitiesJoinHotels;
 };
+Hotels.belongsToMany(Hotels, { through: amenitiesJoinHotels });
+Amenities.belongsToMany(Amenities, { through: amenitiesJoinHotels });
