75592ab3b3f103af2075b767f9da7d2a831374f6
Ananth Rao <ananth3rao24@gmail.com>
2021-04-16 12:33:53 -0400

    small comma changes

--- models/index.js
+++ models/index.js
@@ -1,11 +1,9 @@
 import Hotels from './Hotel.js';
 import Comments from './Comments.js';
 import Locations from './Locations.js';
-import Macros from './Macros.js';
 
 export default {
   Hotels,
   Comments,
   Locations,
-  Macros
 };
